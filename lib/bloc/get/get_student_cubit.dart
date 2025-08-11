465161be6f382a1ad468b6ecaa1e989b201012a9
Phyo Hein ( Ezekiel ) <mwrcps11@gmail.com>
2025-08-11 23:59:55 +0700

    Init Cubit

--- lib/bloc/get/get_student_cubit.dart
+++ lib/bloc/get/get_student_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_rest_api/repository/student_repository.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
@@ -7,5 +8,18 @@ import '../../data/model/student_model.dart';
 part 'get_student_state.dart';
 
 class GetStudentCubit extends Cubit<GetStudentState> {
-  GetStudentCubit() : super(GetStudentInitial());
+  final StudentRepository _studentRepository;
+  GetStudentCubit(this._studentRepository) : super(GetStudentInitial());
+
+  void getAllStudent(){
+    emit(GetStudentInitial());
+    _studentRepository.getAllStudent()
+    .then((value) => 
+      emit(GetStudentSuccess(value))
+    )
+    .catchError((e) => 
+      emit(GetStudentFailure("Student Loading Error"))
+    );
+  }
 }
+
